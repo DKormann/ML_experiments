a1b1b03c0754cf30d7bfb49f5639b034f213584c
dkormann <dominikormann@outlook.com>
2024-06-05 14:37:42 +0000

    display

--- cifar+noise.py
+++ cifar+noise.py
@@ -8,22 +8,23 @@ import matplotlib.pyplot as plt
 from torchvision.datasets import CIFAR10
 
 ds = CIFAR10(root='./datasets',download=True)
+torch.set_default_device('cuda')
 
 #%%
 
-torch.set_default_device('cuda')
 data = torch.tensor(ds.data).float() / 255.
 noise = torch.rand(*data.shape[:-1],1)
-noisy_data = torch.cat([data, noise], dim=-1)
-train_data, test_data = noisy_data.split([40000, 10000])
+add_noise = True
+if add_noise: data = torch.cat([data, noise], dim=-1)
+train_data, test_data = data.split([40000, 10000])
 targets = torch.tensor(ds.targets)
 train_targets, test_targets = targets.split([40000, 10000])
 # %%
 
 class CNN(nn.Module):
-  def __init__(self):
+  def __init__(self, channels=3):
     super(CNN, self).__init__()
-    self.conv1 = nn.Conv2d(4, 64, 3, padding=1)
+    self.conv1 = nn.Conv2d(channels, 64, 3, padding=1)
     self.bn1 = nn.BatchNorm2d(64)
     self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
     self.bn2 = nn.BatchNorm2d(128)
@@ -44,7 +45,7 @@ class CNN(nn.Module):
     x = self.fc2(x)
     return x
 
-net = CNN()
+net = CNN(4)
 assert net(train_data[:5]).shape == torch.Size([5,10])
 opt = torch.optim.Adam(net.parameters(), lr=1e-3)
 
@@ -53,25 +54,41 @@ opt = torch.optim.Adam(net.parameters(), lr=1e-3)
 epochs = 10
 batch_size = 32
 
-for epoch in range(epochs):
-  net.train(True)
-  for i in range(0,len(train_data),batch_size):
-    x = train_data[i:i+batch_size]
-    y = train_targets[i:i+batch_size]
-    opt.zero_grad()
-    out = net(x)
-    loss = F.cross_entropy(out, y)
-    loss.backward()
-    opt.step()
-    print(f'\r{i}/{len(data)}: {loss.item():.5}', end='')
-  net.train(False)
-  with torch.no_grad():
-    out = net(test_data)
-    pred = out.argmax(1)
-    acc = (pred == test_targets).float().mean()
-    loss = F.cross_entropy(out, test_targets)
-    print(f' epoch {epoch} loss: {loss.item():.5} acc: {acc.item():.5}')
-# %%
-channel_weights = net.conv1.weight.square().mean([0,2,3])
 
-plt.bar([0,1,2,3],channel_weights.cpu().detach().numpy())
+results = []
+
+for noisy in range(2):
+
+  runs = []
+  for r in range(4):
+    net = CNN(channels=3 if noisy == 0 else 4)
+    opt = torch.optim.Adam(net.parameters(), lr=1e-3)
+    test_loss = []
+    for epoch in range(epochs):
+      net.train(True)
+      for i in range(0,len(train_data),batch_size):
+        x = train_data[i:i+batch_size,:,:,:3 if noisy == 0 else 4]
+        y = train_targets[i:i+batch_size]
+        opt.zero_grad()
+        out = net(x)
+        loss = F.cross_entropy(out, y)
+        loss.backward()
+        opt.step()
+      net.train(False)
+      with torch.no_grad():
+        out = net(test_data[:,:,:,:3 if noisy == 0 else 4])
+        pred = out.argmax(1)
+        acc = (pred == test_targets).float().mean()
+        loss = F.cross_entropy(out, test_targets)
+      test_loss.append(loss.item())
+    runs.append(test_loss)
+  results.append(runs)
+
+#%%
+for i, result in enumerate(results):
+  for run in result:
+    plt.plot(run, c='r' if i == 0 else 'b')
+plt.xlabel('Epoch')
+plt.ylabel('Test Loss')
+plt.legend(['No Noise']*4+['With Noise']*4)
+plt.show()
